0da43b686d879524402cbc478aa84b074ef296f1
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-10-23 16:56:15 -0600

    Sets up the company model and test

---
+++ spec/models/company_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Company, type: :model do
+  context 'validations' do
+    it {should validate_presence_of(:id)}
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:password)}
+    it {should validate_presence_of(:address)}
+    it {should validate_presence_of(:phone)}
+    it {should validate_presence_of(:team_member_1_name)}
+    it {should validate_presence_of(:team_member_1_title)}
+  end
+end
