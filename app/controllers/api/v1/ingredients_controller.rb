b43b51b97ab177a1c23bf8efc49fce35f95dc5dd
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-10-30 11:09:24 -0600

    Allows ingredients to be changed after schema change

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::IngredientsController < ApplicationController
 
   private
   def ingredient_params
-    params.require(:ingredient).permit(:name, :receiving_from, :receiving_dna, :receiving_other, :receiving_letter, :receiving_hazard_bio, :receiving_hazard_chem, :receiving_hazard_phys, :receiving_hazard_handling, :storage_type, :storage_dna, :storage_other, :storage_hazard_bio, :storage_hazard_chem, :storage_hazard_phys, :storage_hazard_handling, :packaging_dna, :packaging_other, :packaging_method, :packaging_hazard_bio, :packaging_hazard_chem, :packaging_hazard_phys, :packaging_hazard_handling, :preparation_dna, :preparation_other, :preparation_method, :preparation_hazard_bio, :preparation_hazard_chem, :preparation_hazard_phys, :preparation_hazard_handling)
+    params.require(:ingredient).permit(:name, :receiving_from, :receiving_dna, :receiving_other, :receiving_letter, :receiving_hazard_bio, :receiving_hazard_chem, :receiving_hazard_phys, :receiving_hazard_handling, :inventory_type, :inventory_dna, :inventory_other, :inventory_hazard_bio, :inventory_hazard_chem, :inventory_hazard_phys, :inventory_hazard_handling, :packaging_dna, :packaging_other, :packaging_method, :packaging_hazard_bio, :packaging_hazard_chem, :packaging_hazard_phys, :packaging_hazard_handling, :processing_dna, :processing_other, :processing_method, :processing_hazard_bio, :processing_hazard_chem, :processing_hazard_phys, :processing_hazard_handling)
   end
 
 end
